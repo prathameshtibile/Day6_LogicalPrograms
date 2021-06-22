b35046975301ed42a5099ff1a8f29dc32cc000b3
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-06-22 18:44:55 +0530

    Coupon Program added

---
+++ Coupon_code.java
@@ -0,0 +1,33 @@
+/**
+ * @author PRATHAMESH TIBILE
+ * Program to generate a Distinct coupon number.
+ */
+
+
+package Day6_logical;
+
+public class Coupon_code {
+	public static void main(String[] args) {
+		
+		char [] chars = "abcdefghijllmnop". toCharArray();    // char array to get characters for coupen
+		 
+		int max = 10000;                                     //  length of coupen
+		
+		Double d = Math.random();
+		int random = (int) (d*max);
+		String sb = " ";
+		System.out.println("Random Double :" + d +   "Random at start " + random);
+		
+		
+		while (random>0)
+		{
+			sb = sb +(chars[random % chars.length]);
+			random /= chars.length;
+			System.out.println(random + "  " + random % chars.length +  " sb is " +  sb);
+		}
+		
+		String couponCode = sb.toString();
+		System.out.println("Coupon Code: " + couponCode);
+	}
+
+}
